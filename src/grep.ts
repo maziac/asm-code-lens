77e63f9be3f78e6d7809c2397052f836bdd6933d
maziac <thomas.busse@gmx.net>
2018-08-24 18:52:13 +0200

    grep now optionally returns only one result.

--- src/grep.ts
+++ src/grep.ts
@@ -11,7 +11,7 @@ import * as fastGlob from 'fast-glob';
  */
 function read(stream, onData) {
     return new Promise((resolve, reject) => {
-        let promises = [];
+        const promises = [];
         stream.on('data', data => promises.push(onData(data)));
         stream.on('error', reject);
         stream.on('end', () => Promise.all(promises).then(resolve));
@@ -23,26 +23,40 @@ function read(stream, onData) {
  * Searches according to opts.
  * opts includes the directory the glob pattern and the regular expression (the word) to
  * search for.
- * @param opts opts.cwd = directory, opts.globs = the glob pattern, opts.regex = the regular expressionto search for.
+ * @param opts opts.cwd = directory, opts.globs = the glob pattern,
+ * opts.regex = the regular expressionto search for, 
+ * opts.singleResult = true/false, if true only a single result is 
+ * returned (faster).
  * @returns A Map with filenames and matches.
  */
 export async function grep(opts): Promise<Map<string,any>> {
-    let cwd = opts.cwd;
-    let globs = opts.globs;
-    let regex = opts.regex;
+    const cwd = opts.cwd;
+    const globs = opts.globs;
+    const regex = opts.regex;
+    const singleResult = opts.singleResult;
     
-    let readQueue = new PQueue();
-    let fileStream = fastGlob.stream(globs, {cwd: cwd} );
-    let matches = new Map();
+    const readQueue = new PQueue();
+    const fileStream = fastGlob.stream(globs, {cwd: cwd} );
+    const matches = new Map();
+    let leave = false;
     
     await read(fileStream, async fileName => {
+        if(leave)
+            return;
+        
         await readQueue.add(async () => {
+            if(leave)
+                return;
+        
             let filePath = path.join(cwd, fileName);
             let readStream = fs.createReadStream(filePath, { encoding: 'utf-8' });
             let fileMatches = matches.get(fileName);
             let lastIndex = 0;
     
             await read(readStream, data => {
+                if(leave)
+                   return;
+
                 const lines = data.split('\n');
                 const len = lines.length;
                 for (let index = 0; index < len; index++) {
@@ -67,6 +81,12 @@ export async function grep(opts): Promise<Map<string,any>> {
                         end,
                         lineContents,
                     });
+
+                    // Check if only one result is wanted
+                    if(singleResult) {
+                        leave = true;
+                        break;
+                    }
                 }
     
                 lastIndex += len;
